7f0d581de3875aeb470fc3020b18b7c891d816e0
TELECOMM-5 <TELECOMM-5@TELECOMM-05>
2022-12-06 12:37:29 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoMajo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
